3d781c80201752489dc23b0042fd475f1b0d648f
Benjamin Cahn <benjamin.cahn@epitech.eu>
2026-01-19 11:23:44 +0100

    Fin step3 template et view

--- polls/repositories/question_repository.py
+++ polls/repositories/question_repository.py
@@ -1,3 +1,8 @@
-class QuestionRepository:
+from polls.models import Question
 
-    pass
+class QuestionRepository:
+    def get_latest(self, limit=5):
+        return Question.objects.order_by("-pub_date")[:limit]
+
+    def get_by_id(self, question_id):
+        return Question.objects.get(pk=question_id)
--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,52 @@
 from django.shortcuts import render
-
-# Create your views here.
-from django.http import HttpResponse
-
+from django.http import Http404, HttpResponseRedirect
+from django.urls import reverse
+from polls.repositories.question_repository import QuestionRepository
+from polls.models import Choice
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    repo = QuestionRepository()
+    latest_question_list = repo.get_latest()
+    context = {
+        "latest_question_list": latest_question_list
+    }
+    return render(request, "polls/index.html", context)
+def detail(request, question_id):
+    repo = QuestionRepository()
+    try:
+        question = repo.get_by_id(question_id)
+    except Exception:
+        raise Http404("Question does not exist")
+    return render(request, "polls/detail.html", {"question": question})
+def results(request, question_id):
+    repo = QuestionRepository()
+    try:
+        question = repo.get_by_id(question_id)
+    except Exception:
+        raise Http404("Question does not exist")
+    return render(request, "polls/results.html", {"question": question})
+
+def vote(request, question_id):
+    repo = QuestionRepository()
+    try:
+        question = repo.get_by_id(question_id)
+    except Exception:
+        raise Http404("Question does not exist")
+
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "You didn't select a choice.",
+            },
+        )
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(
+            reverse("polls:results", args=(question.id,))
+        )
